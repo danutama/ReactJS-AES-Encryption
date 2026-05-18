3ff04f8d6c144bfc896632b919054df30c444036
Danu Pratama <87930640+danutama@users.noreply.github.com>
2026-05-18 13:23:37 +0700

    Update InputKey.jsx

--- src/components/InputKey.jsx
+++ src/components/InputKey.jsx
@@ -4,7 +4,7 @@ function InputKey({ privateKey, setPrivateKey }) {
   return (
     <div className="mb-3 text-start">
       <label htmlFor="privateKey" className="form-label text-start">
-        Private Key (16/24/32 characters)
+        Private Key (16, 24, or 32 chars)
       </label>
       <input type="text" className="form-control rounded-3 py-2" id="privateKey" value={privateKey} onChange={(e) => setPrivateKey(e.target.value)} placeholder="Enter your private key" />
     </div>
